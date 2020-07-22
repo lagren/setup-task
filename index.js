8dd145e2d97f397257d03732264c3e6b77b29dcf
Eirik Tenold <eirik@relativt.net>
2020-07-22 22:00:30 +0900

    Initial revision

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const tc = require('@actions/tool-cache');
+const core = require('@actions/core');
 
 
 async function run() {
     const taskPath = await tc.downloadTool('https://github.com/go-task/task/releases/download/v2.8.1/task_linux_amd64.tar.gz');
+    const node12ExtractedFolder = await tc.extractTar(taskPath);
+    const cachedPath = await tc.cacheDir(node12ExtractedFolder, "task", "2.8.1");
+    core.addPath(cachedPath);
 }
 
 run();
